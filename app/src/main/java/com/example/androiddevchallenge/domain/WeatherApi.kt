7be29f226bb202fc42b07ac5b6af9c6fec6df4e6
natie <natie.klopper@roamdigital.com>
2021-03-21 18:30:38 +1100

    Spotless

--- app/src/main/java/com/example/androiddevchallenge/domain/WeatherApi.kt
+++ app/src/main/java/com/example/androiddevchallenge/domain/WeatherApi.kt
@@ -37,5 +37,4 @@ object WeatherApiAdapter {
             getOkHttp().build()
         )
     }
-
 }
--- app/src/main/java/com/example/androiddevchallenge/service/Locatonator.kt
+++ app/src/main/java/com/example/androiddevchallenge/service/Locatonator.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.service
 
 import android.Manifest.permission
@@ -38,7 +53,7 @@ class Locatonation(private val ctx: AppCompatActivity) : Locatonator {
                         )
                     }
             } else {
-                //TODO ask to enable location
+                // TODO ask to enable location
                 listen(
                     Pair(
                         -33.879582,
@@ -54,7 +69,7 @@ class Locatonation(private val ctx: AppCompatActivity) : Locatonator {
     private fun isEnabled(): Boolean {
         val locationManager = ctx.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+            locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
     private fun askPermission() {
